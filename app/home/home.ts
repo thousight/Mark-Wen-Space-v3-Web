962bdb2979239722f7449310ceaa06ff261167bb
Mark Wen <markwenguojie94@gmail.com>
2016-07-07 00:33:20 -0400

    Still improving details for responsive design

--- app/home/home.ts
+++ app/home/home.ts
@@ -6,8 +6,7 @@ import { HomeIcons } from './home-icons'
   template: `
             <div class="Home content section scrollspy">
               <h1 class="home-title no-select">Mark Wen</h1>
-              <h3 class="home-subtitle no-select">Web and Mobile</h3><br />
-              <h3 class="home-subtitle no-select">Developer</h3>
+              <h3 class="home-subtitle no-select">Web and Mobile Developer</h3>
               <home-icons></home-icons>
             </div>
             `,
