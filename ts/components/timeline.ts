6fa7ced0c1c77d4092f9aef068231abfbff7b749
Mark Wen <markwenguojie94@gmail.com>
2016-08-17 17:55:14 -0400

    Working on timeline
    Decide if need to use reveal+vertical or horizontal card. Also do styles and offsets on even number cards.

--- ts/components/timeline.ts
+++ ts/components/timeline.ts
@@ -6,7 +6,27 @@ import { Component, Input } from "@angular/core";
               <div class="Timeline">
                 <div class="grayLine"></div>
                 <div *ngFor="let data of data" class="timeline-content">
-                  <!-- {{data.time}} -->
+                  <div class="row">
+                    <div class="col s12 m4 l5">
+                      <div class="card horizontal hoverable">
+                        <div class="card-image waves-effect waves-block waves-light timeline-card-image">
+                          <img class="activator" src="../../{{data.image}}">
+                        </div>
+                        <div class="card-stacked">
+                          <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">{{data.organization}}<i class="material-icons right">more_vert</i></span>
+                            <p>{{data.time}}</p>
+                          </div>
+                          <div class="card-reveal">
+                            <div class="timeline-desc" *ngFor="let bullet of desc">
+                              <i class="material-icons right">close</i>
+                              {{bullet}}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             `
