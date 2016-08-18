c5d460e62d89f900218e25f09e3d29d6bf48bce2
Mark Wen <markwenguojie94@gmail.com>
2016-08-18 02:43:44 -0400

    A lot of Timeline styling

--- ts/components/timeline.ts
+++ ts/components/timeline.ts
@@ -5,24 +5,29 @@ import { Component, Input } from "@angular/core";
   template: `
               <div class="Timeline">
                 <div class="grayLine"></div>
-                <div *ngFor="let data of data" class="timeline-content">
-                  <div class="row">
-                    <div class="col s12 m4 l5">
-                      <div class="card horizontal hoverable">
-                        <div class="card-image waves-effect waves-block waves-light timeline-card-image">
-                          <img class="activator" src="../../{{data.image}}">
-                        </div>
-                        <div class="card-stacked">
-                          <div class="card-content">
-                            <span class="card-title activator grey-text text-darken-4">{{data.organization}}<i class="material-icons right">more_vert</i></span>
-                            <p>{{data.time}}</p>
+                <div class="timeline-content">
+                  <div *ngFor="let data of data">
+                    <div class="row" style="margin-bottom: 0px">
+                      <div class="col s12 m6 l4 offset-l2">
+                        <div class="card horizontal hoverable">
+                          <div class="card-image waves-effect waves-block waves-light">
+                            <img class="timeline-card-image activator" src="../../{{data.image}}" >
+                          </div>
+                          <div class="timeline-card-content card-content">
+                            <span class="card-title grey-text text-darken-4" style="line-height: 25px">{{data.organization}}</span>
+                            <p class="timeline-data-location grey-text text-darken-1" style="margin-top: 5px">{{data.city}}, {{data.state}}</p>
+                            <p style="margin-top: 5px">{{data.title}}</p>
+                            <p style="margin-top: 5px">{{data.time}}</p>
                           </div>
                           <div class="card-reveal">
-                            <div class="timeline-desc" *ngFor="let bullet of desc">
-                              <i class="material-icons right">close</i>
-                              {{bullet}}
+                            <div class="timeline-desc">
+                              <span class="timeline-card-title card-title" style="line-height: 25px">{{data.organization}}<i class="material-icons right">close</i></span>
+                              <ul *ngFor="let bullet of data.desc">
+                                <li>{{bullet}}</li>
+                              </ul>
                             </div>
                           </div>
+                          <i class="timeline-more-button material-icons activator">more_vert</i>
                         </div>
                       </div>
                     </div>
