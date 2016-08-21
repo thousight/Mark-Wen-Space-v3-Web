5ce350083ec6b1880a49dca331b1ccdcae8890ae
Mark Wen <markwenguojie94@gmail.com>
2016-08-21 00:54:46 -0400

    Parallax should be done

--- build/ts/app.js
+++ build/ts/app.js
@@ -9,6 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
 var onLoad_1 = require("./components/onLoad");
 var navBar_1 = require("./components/navBar");
 var home_1 = require("./home");
@@ -16,33 +17,49 @@ var about_1 = require("./about");
 var exp_1 = require("./exp");
 var portfolio_1 = require("./portfolio");
 var edu_1 = require("./edu");
+var skills_1 = require("./skills");
+var contact_1 = require("./contact");
 var App = (function () {
-    function App() {
+    function App(http) {
+        this.http = http;
         this.hidden = true;
+        this.serverLink = "http://mark-wen-space-v3-server.herokuapp.com/allData";
         this.timeout = null;
     }
+    App.prototype.ngOnInit = function () {
+        var _this = this;
+        this.http.get(this.serverLink)
+            .map(function (res) { return res.json(); })
+            .subscribe(function (data) {
+            //  Grabbing all data and pass them to components
+            _this.LinksData = data.Links.sort(function (a, b) { return a.order - b.order; });
+            _this.ExpData = data.Exp.sort(function (a, b) { return a.order - b.order; });
+            _this.PortfolioData = data.Portfolio.sort(function (a, b) { return a.order - b.order; });
+            _this.EduData = data.Edu.sort(function (a, b) { return a.order - b.order; });
+            _this.SkillsData = data.Skills.sort(function (a, b) { return a.order - b.order; });
+            // Change loading screen to main content with 1s delay
+            _this.timeout = setTimeout(function () {
+                _this.hidden = false; // Dismiss loading screen
+                $(".app").fadeIn(1500); // Fading animation
+                clearTimeout(_this.timeout); // Clear out timeout
+            }, 1500);
+        }, function (err) { return console.log(err); });
+    };
     App.prototype.ngAfterContentInit = function () {
         // Initialize Spyscroll
         $(".scrollspy").scrollSpy({
             scrollOffset: 0
         });
-    };
-    App.prototype.ngAfterContentChecked = function () {
-        var _this = this;
-        // Change loading screen to main content with 1s delay
-        this.timeout = setTimeout(function () {
-            _this.hidden = false; // Dismiss loading screen
-            $(".app").fadeIn(1500); // Fading animation
-            clearTimeout(_this.timeout); // Clear out timeout
-        }, 1500);
+        // Initialize Parallax
+        $(".parallax").parallax();
     };
     App = __decorate([
         core_1.Component({
             selector: "app",
-            template: "\n            <!-- List all the main components inside div -->\n            <div class=\"app content\" [hidden]=\"hidden\">\n              <home></home>\n              <navBar></navBar>\n              <about></about>\n              <exp></exp>\n              <portfolio></portfolio>\n              <edu></edu>\n            </div>\n\n            <!-- List other components here -->\n            <onLoad [hidden]=\"!hidden\"></onLoad>\n            ",
-            directives: [onLoad_1.OnLoad, navBar_1.NavBar, home_1.Home, about_1.About, exp_1.Exp, portfolio_1.Portfolio, edu_1.Edu]
+            template: "\n            <!-- List all the main components inside div -->\n            <div class=\"app content\" [hidden]=\"hidden\">\n              <home [data]=\"LinksData\"></home>\n              <navBar></navBar>\n              <about></about>\n              <exp [data]=\"ExpData\"></exp>\n              <!-- <portfolio [data]=\"PortfolioData\"></portfolio> -->\n              <edu [data]=\"EduData\"></edu>\n              <skills [data]=\"SkillsData\"></skills>\n              <div class=\"parallax-container\">\n                <div class=\"parallax\"><img src=\"../img/parallax.jpg\"></div>\n                <div class=\"parallax-text\">\n                  \"Love is patient, love is kind. It does not envy, it does not boast, it is not proud.\n                  It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs.\n                  Love does not delight in evil but rejoices with the truth.\n                  It always protects, always trusts, always hopes, always perseveres.\"\n                  <div>-- NIV, 1 Corithians 13: 4~7</div>\n                </div>\n              </div>\n              <contact></contact>\n            </div>\n\n            <!-- List other components here -->\n            <onLoad [hidden]=\"!hidden\"></onLoad>\n            ",
+            directives: [onLoad_1.OnLoad, navBar_1.NavBar, home_1.Home, about_1.About, exp_1.Exp, portfolio_1.Portfolio, edu_1.Edu, skills_1.Skills, contact_1.Contact]
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], App);
     return App;
 }());
--- ts/app.ts
+++ ts/app.ts
@@ -30,7 +30,7 @@ declare var $: any;
                   It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs.
                   Love does not delight in evil but rejoices with the truth.
                   It always protects, always trusts, always hopes, always perseveres."
-                  <div>--NIV, 1 Corithians 13: 4~7</div>
+                  <div>-- NIV, 1 Corithians 13: 4~7</div>
                 </div>
               </div>
               <contact></contact>
