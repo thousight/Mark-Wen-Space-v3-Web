effad6d7f0605bee04902b208006660f0400f878
Mark Wen <markwenguojie94@gmail.com>
2016-08-28 19:06:53 -0400

    Changed Lite-Server to Express

---
+++ server.js
@@ -0,0 +1,28 @@
+/** ******************** Mark Wen Space v.3 Server Script ****************** **/
+/** ************************************************************************ **/
+
+/** ************************** Base Package Setup ************************** **/
+"use strict"
+var express = require('express')
+var bodyParser = require('body-parser')
+var morgan = require('morgan')
+var app = express()
+// Log every request to the console
+app.use(morgan('dev'))
+// Configure app to use bodyParser(), which will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }))
+// Parse application/json
+app.use(bodyParser.json())
+// Get local files
+process.env.PWD = process.cwd();
+app.use(express.static(process.env.PWD + '/'));
+
+/** **************************** Server Running **************************** **/
+var port = process.env.PORT || 3000
+app.listen(port, err=> {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Listening on port ' + port + '.')
+  }
+})
