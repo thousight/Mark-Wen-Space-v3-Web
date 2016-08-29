78c40fa39bcbaf35bab341b167fb8d93eaff1a31
Mark Wen <markwenguojie94@gmail.com>
2016-08-29 01:17:26 -0400

    Portfolio-list layout almost finished
    Need to design pictures for eaceh portfolio project and add them

--- build/portfolio.js
+++ build/portfolio.js
@@ -18,11 +18,11 @@ var Portfolio = (function () {
     __decorate([
         core_1.Input("data"), 
         __metadata('design:type', Array)
-    ], Portfolio.prototype, "portfolio", void 0);
+    ], Portfolio.prototype, "portfolios", void 0);
     Portfolio = __decorate([
         core_1.Component({
             selector: "portfolio",
-            template: "\n            <div id=\"Portfolio\" class=\"Portfolio content section scrollspy\">\n              <title [icon]=\"titleIcon\" [title]=\"titleTitle\"></title>\n            </div>\n            ",
+            template: "\n            <div id=\"Portfolio\" class=\"Portfolio content section scrollspy\">\n              <title [icon]=\"titleIcon\" [title]=\"titleTitle\"></title>\n              <div class=\"portfolio-list row\">\n                <div class=\"col s12 m12 l8 offset-l2\">\n                  <div class=\"portfolio-card col s12 m5 l5 card hoverable\" [ngClass]=\"{ 'offset-l1 offset-m1': even }\" *ngFor=\"let portfolio of portfolios; let even = even\">\n                    <div class=\"card-image\">\n                      <img src=\"../img/home.jpg\">\n                      <span class=\"card-title\">{{portfolio.title}}</span>\n                    </div>\n                    <div class=\"portfolio-card-content card-content\">\n                      <p>{{portfolio.purpose}}</p>\n                    </div>\n                    <div class=\"card-action\">\n                      <a href=\"#\">Details</a>\n                    </div>\n                  </div>\n                </div>\n              </div>\n            </div>\n            ",
             directives: [title_1.Title]
         }), 
         __metadata('design:paramtypes', [])
--- ts/portfolio.ts
+++ ts/portfolio.ts
@@ -6,6 +6,22 @@ import { Title } from "./components/title";
   template: `
             <div id="Portfolio" class="Portfolio content section scrollspy">
               <title [icon]="titleIcon" [title]="titleTitle"></title>
+              <div class="portfolio-list row">
+                <div class="col s12 m12 l8 offset-l2">
+                  <div class="portfolio-card col s12 m5 l5 card hoverable" [ngClass]="{ 'offset-l1 offset-m1': even }" *ngFor="let portfolio of portfolios; let even = even">
+                    <div class="card-image">
+                      <img src="../img/home.jpg">
+                      <span class="card-title">{{portfolio.title}}</span>
+                    </div>
+                    <div class="portfolio-card-content card-content">
+                      <p>{{portfolio.purpose}}</p>
+                    </div>
+                    <div class="card-action">
+                      <a href="#">Details</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             `,
   directives: [Title]
@@ -14,5 +30,5 @@ import { Title } from "./components/title";
 export class Portfolio {
   private titleIcon = "folder-open";
   private titleTitle = "Portfolio";
-  @Input("data") portfolio: Array<Object>;
+  @Input("data") portfolios: Array<Object>;
 }
