0cb8a9191a073e4f096d7d3983d0b4db115a2467
Mark Wen <markwenguojie94@gmail.com>
2016-09-06 18:17:41 -0400

    Still working on Angular2-Google-Maps

--- build/main.js
+++ build/main.js
@@ -1,12 +1,11 @@
 "use strict";
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var core_1 = require("@angular/core");
-var index_1 = require("angular2-google-maps/core/index");
-var app_1 = require("./app");
+var core_1 = require("angular2-google-maps/core");
+var app_module_1 = require("./app.module");
 // enableProdMode();
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_1.App, [index_1.GOOGLE_MAPS_PROVIDERS, core_1.provide(index_1.LazyMapsAPILoaderConfig, { useFactory: function () {
-            var config = new index_1.LazyMapsAPILoaderConfig();
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule, { provide: (core_1.LazyMapsAPILoaderConfig, { useFactory: function () {
+            var config = new core_1.LazyMapsAPILoaderConfig();
             config.apiKey = "AIzaSyDaHC-kWCYUvx7kgLEFy_pgacY3aeiA5xk";
             return config;
-        } })]);
+        } }) });
 //# sourceMappingURL=main.js.map
--- ts/main.ts
+++ ts/main.ts
@@ -1,12 +1,12 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { enableProdMode, provide } from "@angular/core";
-import { LazyMapsAPILoaderConfig } from "angular2-google-maps/core/index";
-import { App } from "./app";
+import { enableProdMode } from "@angular/core";
+import { LazyMapsAPILoaderConfig } from "angular2-google-maps/core";
+import { AppModule } from "./app.module";
 
 // enableProdMode();
 
-platformBrowserDynamic().bootstrapModule(App, [provide(LazyMapsAPILoaderConfig, {useFactory: () => {
+platformBrowserDynamic().bootstrapModule(AppModule, {provide: (LazyMapsAPILoaderConfig, {useFactory: () => {
     let config = new LazyMapsAPILoaderConfig();
     config.apiKey = "AIzaSyDaHC-kWCYUvx7kgLEFy_pgacY3aeiA5xk";
     return config;
-  }})]);
+  }})});
